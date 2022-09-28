3b5caf3ac0be5fc6e9b81304b7d4c8cbff19081a
松井貴史 <>
2022-09-28 21:04:27 +0900

    update:サービスページ作成

--- page-service.php
+++ page-service.php
@@ -12,19 +12,27 @@ Template Post Type: page
 <main role="main">
     <section class="p-content" id="intro">
         <div class="p-content-head --main">
-            <h2 class="title --main">２つのシンプルなサービスメニュー</h2>
-            <p class="lead --main">多くのサービスメニューは内容が細かく複雑でわかりづらい。<br>伝わらなければ意味がないし決断も鈍る。だからこそできるだけシンプルを心がけています。</p>
+            <h2 class="title --main">２つのサービスメニュー</h2>
+            <p class="lead --main">多くのサービスメニューは内容が細かく複雑でわかりづらいものでした。<br>伝わらなければ意味がないし決断も鈍る。だからこそできるだけやる事を絞りシンプルにを心がけています。</p>
         </div>
 
         <div class="p-content-cards">
             <div class="p-content-card">
-                <h3>顧問型・パートナー型契約</h3>
-                <p class="p-content-card__text">弊社の強みはデザインからシステムまでをワンストップで提供できること。<br>新規サービス、社内業務システム開発、デザイン・開発会社向け受託案件サポート、WEBコンサルなど御社の求められることに柔軟に対応いたします。 <br>例えば、新しくサービスを立ち上げたい、問題点から一緒に解決案を考えてほしい。といったようなケースにも対応できます。</p>
+                <h3>1. 顧問型・パートナー型契約</h3>
+                <p class="p-content-card__text">弊社の強みはデザインからシステムまでをワンストップで提供できること。<br>新規サービス、社内業務システム開発、デザイン・開発会社向け受託案件サポート、WEBコンサルなど御社の求められることに柔軟に対応いたします。 </p>
+                <dl class="use-case">
+                    <dt class="use-case__label">利用ケース</dt>
+                    <dd class="use-case__text">新しくサービスを立ち上げたい、問題点から一緒に解決案を考えてほしい、作りながら考えたい、意見をもらいたい</dd>
+                </dl>
             </div>
 
             <div class="p-content-card">
-                <h3>納品完結型</h3>
+                <h3>2. 納品完結型</h3>
                 <p class="p-content-card__text">一般的な作って終わりの制作スタイルです。<br>パートナー型をメインとしているため月にお受けできる総数は２件までとなります。その時の状況により左右されますので、お早めにご連絡ください。</p>
+                <dl class="use-case">
+                    <dt class="use-case__label">利用ケース</dt>
+                    <dd class="use-case__text">作るところだけをお願いしたい、指示通りに進行して欲しい</dd>
+                </dl>
             </div>
         </div>
 
@@ -35,34 +43,203 @@ Template Post Type: page
         </div>
 
         <div class="problem-items">
-            <div class="problem-item">
-                <span class="lead">問題１：知らされない線引き・積みかなさなるバッファ</span>
-                <h3>そこからは別途でかかります!</h3>
-                <p>従来の納品型では見積もりに乗っていない依頼に関しては別途費用がかかってしまいます。仕様を決定した上で見積もりを算出するため、これもお願いみたいなことは基本的にできません。</p>
-                <p>その結果、追加費用が重なる。もしくは費用がなければ追加できず思うよな効果をえられません。</p>
+            <div class="problem-item p-content-card">
+<!--                <span class="lead">問題１：仕様を決めるのが難しい・積み重なるバッファによるコスト高</span>-->
+                <h3>
+<!--                    そこからは別途でかかります!-->
+                    問題１：仕様を決めるのが難しい・積み重なるバッファによるコスト高
+                </h3>
+                <p>従来の納品型では見積もりに乗っていない依頼に関しては別途費用がかかってしまいます。仕様を決定した上で見積もりを算出するため、良いアイデアを思いついても別途お積りとなる場面が多くありました。</p>
+                <p>その結果、追加で費用が重なりコストが嵩んでしまいます。もしくは費用がなければ追加できず思うよな効果をえられません。</p>
+                <p><strong>顧問型では、仕様を決めながら進行するため必要な時に必要な物を作れます。</strong>必要最低限のものでスタートさせ、仮説検証をしながらその都度仕様をブラッシュアップしていくことができます。</p>
+                <p>そのため、コストを抑えながら効果手なPMFを実現できます。</p>
             </div>
 
-            <div class="problem-item">
-                <span class="lead">問題2:仕様変更が難しい</span>
-                <h3>仕様変更であれば、また別でお見積りさせて頂きます。</h3>
-                <p>従来の納品型の場合は、見積もり段階で費用を算出するため、見積もりに乗っていない依頼に関しては別途費用がかかります。これもお願いみたいなことは基本的にできません。</p>
+            <div class="problem-item p-content-card">
+<!--                <span class="lead">問題2：事業理解やサービス理解に時間かかる</span>-->
+                <h3>
+<!--                    事業の事を一から説明するのは時間がかかる・・-->
+                    問題2：事業理解やサービス理解に時間かかる
+                </h3>
+                <p>外注の課題は事業説明やサービス内容の説明に時間がかかってしまう事。デザインやシステム開発をお願いする場合、意図や事業の理解が必要で一番労力がかかります。</p>
+                <p>事業の理解がなければ意思疎通に時間かかり、結果その時間分のコストがかかってしまいます。</p>
+                <p>顧問型の場合、導入当初は理解へのコストはかかるものの、どんどん説明が不要になり、プロジェクト進行中に新しくサービスを作る場合でも事前情報がなくても意思疎通が取れ、コンテンツ事態の質も向上できます。</p>
             </div>
 
-            <div class="problem-item">
-                <span class="lead">問題3:見積もりと稟議によるスピードダウンでビジネスチャンスの機会損失</span>
-                <h3>稟議にかける必要があるので先に見積もり出してもらっていいでしょうか？</h3>
-                <p>従来の納品型の場合は、見積もり段階で費用を算出するため、見積もりに乗っていない依頼に関しては別途費用がかかります。これもお願いみたいなことは基本的にできません。</p>
+            <div class="problem-item p-content-card">
+<!--                <span class="lead">問題3：仕様変更が難しい</span>-->
+                <h3>
+<!--                    仕様変更であれば、また別でお見積りさせて頂きます。-->
+                    問題3：仕様変更が難しい
+                </h3>
+                <p>従来の納品型の場合は、見積もり段階で仕様を固め費用を算出する必要があります。しかし、最初から全ての仕様を決めるのは非常に困難で無駄が発生しやすいです。</p>
+                <p>顧問型の場合、進行中であっても良いアイデアがでれば採用ができます。より目的に沿った効果的なWEBサイト運用やシステム開発が可能になります。</p>
             </div>
 
-            <div class="problem-item">
-                <span class="lead">問題4:作って終わり・提案をくれない・指示待ち発生で費用対効果が望めない</span>
-                <h3>納品完了となります。また仕様や指示ありましたらご連絡ください。</h3>
-                <p>従来の納品型の場合は、見積もり段階で費用を算出するため、見積もりに乗っていない依頼に関しては別途費用がかかります。これもお願いみたいなことは基本的にできません。</p>
+            <div class="problem-item p-content-card">
+<!--                <span class="lead">問題4：稟議によるスピードダウンとビジネスチャンスの機会損失</span>-->
+                <h3>
+<!--                    稟議にかける必要があるので先に見積もり出してもらっていいでしょうか？-->
+                    問題4：稟議によるスピードダウンとビジネスチャンスの機会損失
+                </h3>
+                <p><span>WEBを取り巻く環境は、受託が一般的でした。</span>予算ありきの制作では線引きや双方の目的のズレが生まれてしまい、修正やバッファが嵩むとその度に、請求書をあげ稟議にかける必要があり、タイムロスが生じてしまいます。</p>
+                <p>顧問型の場合、毎月定額制で関わるため稟議書類が不要になります。その結果、素早い判断とアクションに繋がるスモールスタートを実現できます。</p>
+            </div>
+
+            <div class="problem-item p-content-card">
+<!--                <span class="lead">問題5：作って終わりでは効果が見込めない</span>-->
+                <h3>
+<!--                    納品完了となります。また仕様や指示ありましたらご連絡ください。-->
+                    問題5：作って終わりは効果が見込めない
+                </h3>
+                <p>従来の制作の場合は、仕様に沿って作り納品すれば完了です。指示内容に沿って進行し完了すれば、それ以降の責任を追うことはありません。</p>
+                <p>ですがWEBサービスやシステム開発は作ってからがスタートで常にフィードバックと改善を行い向上する必要性があります。そのため従来の納品型では成果を出すこと事が難しいのです。</p>
             </div>
         </div>
     </section>
 
-    <section class="p-content" id="intro"></section>
+    <section class="p-content" id="solution">
+        <div class="p-content-head">
+            <h2 class="title">提供できるもの</h2>
+        </div>
+
+        <div class="solution-item">
+            <h3>スキル（業務歴10年以上）</h3>
+            <p>Laravel/ PHP / MySQL / HTML / CSS / SCSS / Javascript / jQuery / Figma / Illustrator / Photoshop / 改善運用・提案</p>
+        </div>
+
+        <div class="solution-item">
+            <h3>できること</h3>
+            <ul>
+                <li>常に目的思考で考え、緊張感を持ち費用以上のパフォーマンスをお返しできるように全力で取り組みます。</li>
+                <li>作業要因としての仕事ではなく、お客様にとって良い事・利益にならない事、お互いの信頼関係を築くために必要な事であれば意見いたします。</li>
+            </ul>
+        </div>
+
+        <div class="solution-item">
+            <h3>できないこと</h3>
+            <ul>
+                <li>会社に直接訪問はいたしません。</li>
+                <li>土日祝日深夜の対応は行っておりません。</li>
+                <li>急な依頼や短納期のご依頼はお受けできません。</li>
+                <li>詳しくは<a href="/guide" target="_blank">よくある質問</a>をご確認ください。</li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="p-content" id="price">
+        <div class="p-content-head">
+            <h2 class="title">料金プラン</h2>
+        </div>
+
+        <div class="p-content-card__border plan-item">
+            <h4 class="plan-item__course">ライト</h4>
+            <div class="plan-item__price">
+                <span class="number">100,000</span>
+                <span class="unit">月/税別</span>
+            </div>
+            <dl class="plan-item__time flex">
+                <dt class="label">月内の稼働時間目安:</dt>
+                <dd class="time">10時間</dd>
+                <dd class="weekly-time">週2時間程度</dd>
+            </dl>
+            <span class="plan-item__status flex"><i class="ri-check-line u-color__green"></i>相談・申込み可能</span>
+        </div>
+
+        <div class="p-content-card__border --focus plan-item">
+            <h4 class="plan-item__course">スタンダード <span class="u-tag__recommend">おすすめ</span></h4>
+            <div class="plan-item__price">
+                <span class="number">200,000</span>
+                <span class="unit">月/税別</span>
+            </div>
+            <dl class="plan-item__time flex">
+                <dt class="label">月内の稼働時間目安:</dt>
+                <dd class="time">20時間</dd>
+                <dd class="weekly-time">週4時間程度</dd>
+            </dl>
+            <span class="plan-item__status flex"><i class="ri-check-line u-color__green"></i>相談・申込み可能</span>
+        </div>
+
+        <div class="p-content-card__border plan-item">
+            <h4 class="plan-item__course">プレミアム</h4>
+            <div class="plan-item__price">
+                <span class="number">300,000</span>
+                <span class="unit">月/税別</span>
+            </div>
+            <dl class="plan-item__time flex">
+                <dt class="label">月内の稼働時間目安:</dt>
+                <dd class="time">30時間</dd>
+                <dd class="weekly-time">週８時間程度</dd>
+            </dl>
+            <span class="plan-item__status flex"><i class="ri-check-line u-color__green"></i>相談・申込み可能</span>
+        </div>
+
+        <div class="p-content-card__border plan-item">
+            <h4 class="plan-item__course">納品完結型</h4>
+            <p class="plan-item__description">費用やスケジュールなどはご相談の上決まります。</p>
+            <span class="plan-item__status flex"><i class="ri-check-line u-color__green"></i>残り1件</span>
+        </div>
+
+    <section class="p-content" id="user-voice">
+        <div class="p-content-head">
+            <h2 class="title">お客様の声</h2>
+        </div>
+
+        <div class="p-content-card user-voice-item">
+            <div class="user-voice-item__head flex">
+                <span class="user-voice-item__avatar"><img src="<?php echo get_template_directory_uri(); ?>/img/avatar/irie_shingo.jpg" alt="入江慎吾" loading="lazy"></span>
+                <div class="user-voice-item__name">
+                    <b class="company-name">MENTA株式会社</b>
+                    <p class="name">代表取締役社長 入江 慎吾</p>
+                </div>
+            </div>
+            <div class="user-voice-item__body">
+                <p>松井さんとは数年一緒に仕事をしていますが、単なる職種としてのデザイナーではくくれない方です。ビジネス思考を持ち、必要ならプログラミングもやってしまう。利用者であるユーザーが求めていることを知り、一緒に対話しながら解決策を考えていく。あくまでもその手段がデザインであるという考えです。逆に言えば、決まった仕様や内容をデザインするだけでは松井さんにお願いするのはもったいないなと思います。一緒に信頼してビジネスができるパートナーです。</p>
+            </div>
+        </div>
+
+        <div class="p-content-card user-voice-item">
+            <div class="user-voice-item__head flex">
+                <span class="user-voice-item__avatar"><img src="<?php echo get_template_directory_uri(); ?>/img/avatar/yoshimura_tomohiro.jpg" alt="吉村知宏" loading="lazy"></span>
+                <div class="user-voice-item__name">
+                    <b class="company-name">株式会社MONOBANK</b>
+                    <p class="name">鑑定士 吉村知宏</p>
+                </div>
+            </div>
+            <div class="user-voice-item__body">
+                <p>松井さんは数年弊社のデザイナーとして仕事をしてくれてます。
+                    私自身、WEBについて知識の疎い部分があるのですがそんな私にも分かりやすい言葉で説明してくれ、大変助かっています。
+                    実際のお仕事も見た目のデザインだけではなく、対話の中でこちらの意図をくみ取り集客までのデザインも構築してくれます。
+                    まさにプロの仕事をしてくれるかけがえのないパートナーだと思っています。
+                </p>
+            </div>
+        </div>
+
+        <div class="p-content-card user-voice-item">
+            <div class="user-voice-item__head flex">
+                <span class="user-voice-item__avatar"><img src="<?php echo get_template_directory_uri(); ?>/img/avatar/suefuji_wakako.jpg" alt="末藤 和佳子" loading="lazy"></span>
+                <div class="user-voice-item__name">
+                    <b class="company-name">ご一緒したメンバーの声</b>
+                    <p class="name">UXデザイナー 末藤 和佳子</p>
+                </div>
+            </div>
+            <div class="user-voice-item__body">
+                <p>ビジネス面での課題をお持ちのクライアント案件で真っ先にお声かけするのが松井さんです。
+                    これまで、いくつかのプロジェクトでご一緒させて頂きましたが、ビジネス、クリエイティブ、テクノロジーと多方面に明るく、まさにBTC人材であると感じております。
+                    また、ご自身でもプロダクトを開発しサービス化されている為、顧客視点とマーケット視点とをバランス良く持たれており提案のアウトカムも顧客満足度の高いものとなっています。
+                    今後、益々活動の領域を広げられると期待しています。
+                </p>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="p-content contact" id="contact">
+        <div class="p-content-head">
+            <h2 class="title">お問合せ先</h2>
+        </div>
+        <p class="contact-text">興味をもたれましたら、こちらの<a href="/contact" target="_blank">お問合せフォーム</a>よりご連絡お願いいたします。</p>
+    </section>
 </main>
 <?php get_footer(); ?>
 
